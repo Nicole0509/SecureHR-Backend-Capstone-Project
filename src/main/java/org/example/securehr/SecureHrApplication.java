2d5ed9696da24f34ba278efd7c06b3b8b336739f
Nicole0509 <niashavril0509@gmail.com>
2025-11-14 11:53:45 +0200

    Initial commit

---
+++ src/main/java/org/example/securehr/SecureHrApplication.java
@@ -0,0 +1,13 @@
+package org.example.securehr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureHrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureHrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/securehr/SecureHrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.securehr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureHrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
